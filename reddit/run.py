c4eee84bb4a026368938fcaf997828bf719ae8bd
ankman007 <ankeetpoudel6@gmail.com>
2025-03-24 12:20:41 +0545

    added-pagination-in-reddit-script

--- reddit/run.py
+++ reddit/run.py
@@ -1,11 +1,12 @@
 import json
 import os 
+import time
 import requests
-from reddit.utils import get_post_comments
+from bs4 import BeautifulSoup
 from reddit.constant import search_queries, subreddits
 
 
-def scrape(folder_name=None):
+def scrape_posts(folder_name=None, pages=3, subreddits=["localseo"]):
     base_url = "https://old.reddit.com/r/"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
@@ -13,58 +14,83 @@ def scrape(folder_name=None):
         folder_name = os.path.join(os.path.dirname(__file__), "data")
 
     os.makedirs(folder_name, exist_ok=True)
+    all_posts = []
 
     for subreddit in subreddits:
-        url = base_url + subreddit + ".json"
-        response = requests.get(url, headers=headers)
+        pagination_token = None
+        for _ in range(pages):
+            url = f"{base_url}{subreddit}.json"
+            if pagination_token:
+                url += f"?after={pagination_token}"
+                
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                print(f"Error {response.status_code} for {url}")
+                break  
+            
+            content = response.json()
+            posts = content["data"]["children"]
+            for post in posts:
+                post["data"]["subreddit"] = subreddit
+                all_posts.append(post["data"])
+            
+            pagination_token = content["data"]["after"]
+            if not pagination_token:
+                break 
+            time.sleep(3)
+            
+    file_path = os.path.join(folder_name, "posts.json")
+    with open(file_path, "w", encoding="utf-8", newline="") as file:
+        json.dump(all_posts, file, indent=4)
+    print(f"Saved: {file_path}")
 
-        file_name = f"{subreddit}.json"
-        file_path = os.path.join(folder_name, file_name)
 
-        content = response.json()
-        with open(file_path, "w", encoding="utf-8", newline="") as file:
-            json.dump(content, file, indent=4)
-        print(f"Saved: {file_path}")
+# def scrape_comments(url):
+#     post_comments = []
+#     response = requests.get(url)
+
+#     html = response.content
+
+#     soup = BeautifulSoup(html, 'html.parser')
+#     content = soup.find_all(class_="comment")
+
+#     for item in content[1:]:
+#         comments = item.find_all(class_="usertext-body")
+#         for comment in comments:
+#             text = comment.get_text(strip=True)  
+#             post_comments.append(text)  
+#     return post_comments 
 
 
 def analyze(search_queries):
     folder_name = os.path.join(os.path.dirname(__file__), "data")
-    if not os.path.exists(folder_name):
-        print(f"Error: Data folder '{folder_name}' does not exist. Run scrape() first.")
-        return
+    file_path = os.path.join(folder_name, "posts.json")
 
-    file_list = os.listdir(folder_name)
     matching_posts = []
     formatted_search_queries = [query.lower() for query in search_queries]  
     
-    for file in file_list:
-        folder_path = os.path.join(folder_name, file)
-        with open(folder_path, "r", encoding="utf-8") as file:
-            data = json.load(file)
-            
-        children = data["data"]["children"]
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        
 
-        for child in children:
-            post_data = child['data']
-            title = post_data['title']
-            post_description = post_data['selftext']
-            permalink = post_data['url']
+    for post_data in data:
+        title = post_data['title']
+        post_description = post_data.get("selftext", "")
+        permalink = "https://www.reddit.com" + post_data.get("permalink", "")
 
-            if any(query in title.lower() or query in post_description.lower() for query in formatted_search_queries):
-                matching_posts.append(child)
-                continue  
-
-            comments = get_post_comments(permalink)
-            if any(query in comment.lower() for query in formatted_search_queries for comment in comments):
-                matching_posts.append(child)
+        if any(query in title.lower() or query in post_description.lower() for query in formatted_search_queries):
+            post_data["full_permalink"] = permalink  
+            matching_posts.append(post_data)
                 
-    output_file = os.path.join(os.path.dirname(__file__), "data/relevant_posts.json")            
+    output_file = os.path.join(os.path.dirname(__file__), "data/output.json")            
     with open(output_file, "w", encoding="utf-8") as outfile:
         json.dump(matching_posts, outfile, indent=4)
 
-    print(f"{len(matching_posts)} matching posts saved to matching_posts.json", )
+    print(f"{len(matching_posts)} matching posts saved to output.json")
+    
     
 if __name__ == "__main__":
-    # scrape()
+    scrape_posts(subreddits=subreddits)
+    # scrape_comments()
     analyze(search_queries)
     pass 
